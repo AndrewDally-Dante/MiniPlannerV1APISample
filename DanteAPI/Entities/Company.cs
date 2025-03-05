ba99bcf439e78d044bdb1c676baa8aefd4786d2a
Andrew Dally <andrew@dante.co.uk>
2025-03-05 16:54:29 +0000

    Added nuget instructions and published package to nuget

--- DanteAPI/Entities/Company.cs
+++ DanteAPI/Entities/Company.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DanteAPI.Entities
+﻿namespace DanteAPI.Entities
 {
     public  class Company
     {
---
+++ DanteAPI/NugetReadme.txt
@@ -0,0 +1,4 @@
+﻿Update version in ths project file
+Open CMD/Terminal in this projects folder
+dotnet pack -c Release
+dotnet nuget push bin/Release/Dante.Api.1.0.*.nupkg --api-key *** --source https://api.nuget.org/v3/index.json
