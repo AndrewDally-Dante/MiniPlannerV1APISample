bee94364fa73c2cdace2440aaa6a5cfeeada7062
Andrew <Andrew@dante.co.uk>
2025-08-12 09:11:14 +0100

    Added generic item and resource types

---
+++ DanteAPI/Lookups/GenericItemTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace DanteAPI.Lookups
+{
+    public enum GenericItemTypes
+    {
+        Fee_Types = 1,
+        Results = 2,
+        Enquiry_Sources = 3,
+        Genders = 4,
+        Appointment = 5,
+        Closed_Lost_Reason = 6,
+        Sites = 7,
+        Ethnicities = 8,
+        Course_Cost_Types = 9,
+        Company_Types = 10,
+        Resource_Group = 11
+    }
+}
---
+++ DanteAPI/Lookups/ResourceTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace DanteAPI.Lookups
+{
+    [Flags]
+    public enum ResourceTypes
+    {
+        Venue = 1,
+        Tutor_Internal = 2,
+        Tutor_External = 4,
+        Assessor = 8,
+        Verifier = 16,
+        Other = 32,
+        Equipment = 64,
+        Machinery = 128,
+        Training_Provider = 256
+    }
+}
